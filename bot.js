b0dcda547d203b127e4fc9dffea45214377cf3e5
zakisghir123 <57421107+zakisghir123@users.noreply.github.com>
2019-11-06 01:48:41 +0100

    Add files via upload

--- bot.js
+++ bot.js
@@ -23,7 +23,7 @@ client.on('ready', () => {
       console.log(`ON ${client.guilds.size} Servers '     Script By : i1Suhaib ' `);
     console.log(`----------------`);
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`Ali`,"http://twitch.tv/S-F")
+client.user.setGame(`.Eurre...By AaB`,"http://twitch.tv/S-F")
 client.user.setStatus("dnd")
 });
 
